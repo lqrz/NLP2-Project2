f4bda541f3fdbba42b387d1ee39e40e402b08e20
Roger Wechsler <mr.mutator@gmail.com>
2015-05-14 12:37:11 +0200

    Load model

--- word2vec.py
+++ word2vec.py
@@ -11,9 +11,18 @@ def train_vector_representations(corpus_file, output_file, min_count=5, vector_d
 
 
 
+
+
 if __name__ == "__main__":
 
+    # train and save model
     train_vector_representations("data/europarl-v7.en.gz", "models/en.w2v", min_count=5, vector_dim=400, workers=3)
+
+    # load model
+    model = gensim.models.Word2Vec.load('models/en.w2v')
+
+
+
 
 
 
