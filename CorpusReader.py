ad929e8df69b9d6dbff8da71e711fab73607de9f
Roger Wechsler <mr.mutator@gmail.com>
2015-05-14 10:20:26 +0200

    Simple training script for monolingual vector representations

--- CorpusReader.py
+++ CorpusReader.py
@@ -1,4 +1,5 @@
 import gzip
+import nltk
 
 
 class CorpusReader():
@@ -6,21 +7,26 @@ class CorpusReader():
     Reads corpus from gzip file.
     """
 
-    def __init__(self, file_name):
+    def __init__(self, file_name, max_limit=None):
         self.file = gzip.open(file_name, "r")
+        self.max_limit = max_limit
 
 
     def __iter__(self):
         self.file.seek(0)
+        c = 0
         for line in self.file:
-            yield line.split()
+            c += 1
+            if self.max_limit and c > self.max_limit:
+                break
+            yield nltk.word_tokenize(line.decode("utf-8"))
 
 
 
 
 
 if __name__ == "__main__":
-    cr = CorpusReader("data/europarl-v7.en.gz")
+    cr = CorpusReader("data/europarl-v7.en.gz", max_limit=100)
     for sent in cr:
         print sent
 
--- word2vec.py
+++ word2vec.py
@@ -1,15 +1,19 @@
 import gensim
+from CorpusReader import CorpusReader
+
+def train_vector_representations(corpus_file, output_file, min_count=5, vector_dim=400, workers=1):
+    cr = CorpusReader(corpus_file)
+
+    model = gensim.models.Word2Vec(cr, min_count=min_count, size=vector_dim, workers=workers)
+    model.save(output_file)
 
 
 
 
 
 if __name__ == "__main__":
-    from CorpusReader import CorpusReader
-    cr = CorpusReader("data/europarl-v7.en.gz")
 
-
-    model = gensim.models.Word2Vec(cr, min_count=1, size=400, workers=2)
+    train_vector_representations("data/europarl-v7.en.gz", "models/en.w2v", min_count=5, vector_dim=400, workers=3)
 
 
 
