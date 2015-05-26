51af8ee5203fad342c49cded8df1fff407f56ee4
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2015-05-26 14:12:11 +0200

    load word2vec models

--- translationMatrix.py
+++ translationMatrix.py
@@ -9,46 +9,51 @@ from sklearn.linear_model import Ridge
 
 if __name__ == '__main__':
 
-	#------------- Begin params -------------#
-	nWords = 5 # Number of words to train translation matrix
-	nTranslations = 1
-	alpha = 0.1 # linear regression solver regularization param
-	# path = sys.argv[1] # path to .norm file (enlgish)
-	# modelPath = sys.argv[2] # path to model file (word2vec)
-	modelPath = '/'
-	path = '/home/lqrz/Desktop/fold/europarl.en.norm' # path to .norm file (enlgish)
-	#------------- End params -------------#
+    # ------------- Begin params -------------#
+    nWords = 5  # Number of words to train translation matrix
+    nTranslations = 1
+    alpha = 0.1  # linear regression solver regularization param
+    # path = sys.argv[1] # path to .norm file (enlgish)
+    # modelPath = sys.argv[2] # path to model file (word2vec)
+    modelPath = '/'
+    # path = '/home/lqrz/Desktop/fold/europarl.en.norm'  # path to .norm file (enlgish)
+    path = 'data/en/norm/lqrz.en'  # path to .norm file (enlgish)
+    # ------------- End params -------------#
 
-	idx = path.rfind('/')+1
-	folder = path[0:idx]
-	filename = path[idx:]
+    idx = path.rfind('/') + 1
+    folder = path[0:idx]
+    filename = path[idx:]
 
-	wordlist = PlaintextCorpusReader(folder,filename)
+    wordlist = PlaintextCorpusReader(folder, filename)
 
-	# Construct frequency dictionary
-	fd = FreqDist(wordlist.words())
+    # Construct frequency dictionary
+    fd = FreqDist(wordlist.words())
 
-	# Get translations from api En -> De
-	translations = dict()
+    # Get translations from api En -> De
+    translations = dict()
 
-	mostFrequentWords = sorted(fd.items(),key=lambda x:x[1],reverse=True)[:nWords]
+    mostFrequentWords = sorted(fd.items(), key=lambda x: x[1], reverse=True)[:nWords]
 
-	model = loadModel()
+    # Load word2vec trained models
+    gensimModelEn = loadModel('models/mono_800_en.bin')
+    gensimModelDe = loadModel('models/mono_200_de.bin')
 
-	for word,_ in mostFrequentWords:
+    for word, _ in mostFrequentWords:
 
-		# Get translation from thesaurus api
-		translations[word] = get_translations(word)[:nTranslations]
+        # Get translation from thesaurus api
+        translations[word] = get_translations(word)[:nTranslations]
 
-		# Load word2vec trained models (En, De)
+        # Get representations for words
+        wordRepEn = gensimModelEn[word]
 
-		# Get representations for words
+        for trans in translations[word]:
+            wordRepDe = gensimModelDe[trans]
 
-		# Instantiate linear regression solver
-		solver = Ridge(alpha,solver='lsqr')
-		
-		# Fit data
-		solver.fit(X,y)
+    # Instantiate linear regression solver
+    solver = Ridge(alpha, solver='lsqr')
 
-		# Store translation matrix
-		solver.coef_
+    # Fit data
+    solver.fit(X, y)
+
+    # Store translation matrix
+    solver.coef_
