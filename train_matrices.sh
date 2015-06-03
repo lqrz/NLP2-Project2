f823a4b76e985ac90f123dd94accbde0b91856d0
Roger Wechsler <mr.mutator@gmail.com>
2015-06-03 12:32:57 +0200

    Removed echo.

--- train_matrices.sh
+++ train_matrices.sh
@@ -3,7 +3,7 @@ do
   for dedim in 200 400 800
   do
     echo "Working on EN=${endim}, DE=${dedim}"
-    echo "python translationMatrix2.py models/truecase/top10ken_trans_yandex.txt models/truecase/mono_${endim}_en.bin models/truecase/mono_${dedim}_de.bin models/truecase/tm_${endim}_${dedim}.p > results/log_${endim}_${dedim}.txt"
+    python translationMatrix2.py models/truecase/top10ken_trans_yandex.txt models/truecase/mono_${endim}_en.bin models/truecase/mono_${dedim}_de.bin models/truecase/tm_${endim}_${dedim}.p > results/truecase/log_${endim}_${dedim}.txt
   done
 done
 echo "done"
