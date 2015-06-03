f0a27c9316b717833f1b951daa239693ffba1782
Roger Wechsler <mr.mutator@gmail.com>
2015-06-03 12:22:39 +0200

    Included cross-validation for alphas.

--- translationMatrix2.py
+++ translationMatrix2.py
@@ -3,8 +3,9 @@ import codecs
 import gensim
 import numpy as np
 import pickle
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, RidgeCV
 import sys
+from collections import Counter
 
 
 def get_dict_samples(dict_file):
@@ -36,6 +37,7 @@ def evaluate(test_samples, en_model, de_model, translation_matrix, k_nearest=1, 
     """
     found_similarities = []
     candidate_translations = []
+    ranks = []
     for word, translations in test_samples:
             # Get representations for words
             test_rep = en_model[word]
@@ -50,41 +52,41 @@ def evaluate(test_samples, en_model, de_model, translation_matrix, k_nearest=1, 
                     if gold_trans == pred_trans:
                         found_transl += 1
                         similarity_sum += similarities[i]
+                        ranks.append(i+1)
 
             if found_transl:
                 found_similarities.append(float(similarity_sum) / found_transl)
             else:
+                ranks.append(0)
                 found_similarities.append(0)
 
-    return found_similarities, candidate_translations
+    return found_similarities, candidate_translations, ranks
 
-def calculate_accuracy(similarities):
-    total = 0
-    correct = 0
-    sum = 0
-    for sim in similarities:
-        total += 1
-        if sim > 0:
-            correct += 1
-            sum += sim
+def calculate_acc_avgsim(similarities, ranks):
 
-    accuracy = float(correct) / total if total > 0 else 0
-    average_sim = float(sum) / correct if correct > 0 else 0
+    total = len(ranks)
+    c = Counter(ranks)
 
-    return accuracy, average_sim
+    counts = [float(c.get(r, 0)) for r in range(1, np.max(c.keys()))]
+    accuracies = np.cumsum(counts) / float(total)
 
-def learn_translation_matrix(X, Y, alpha=0.1):
-    print "Learning translation matrix"
+    average_sim = np.mean([s for s in similarities if s > 0])
+    average_rank = np.mean([r for r in ranks if r > 0])
+
+    return accuracies, average_sim, average_rank
+
+def learn_translation_matrix(X, Y, alphas=[0.1, 1.0, 10.0]):
     # Instantiate linear regression solver
-    solver = Ridge(alpha, solver='lsqr')
+    solver = RidgeCV(alphas=alphas, cv=None, fit_intercept=True, scoring=None,
+    normalize=False)
 
     # Fit data
     solver.fit(X, Y)
 
+    print "Best alpha is ", solver.alpha_
+
     # translation_matrix
-    return solver.coef_
-
-
+    return solver.coef_, solver.alpha_
 
 
 def get_valid_samples(samples, n, model, n_translations=None):
@@ -126,30 +128,33 @@ def get_train_test_sets(dict_file, n_translations, n_train_samples, n_test_sampl
 
 
 if __name__ == '__main__':
+    # Either runs parameters specified here or the ones in the command-line.
 
     # ------------- Begin params -------------#
-    #matrix_file = "models/lowercase/800_200_tm.p"
+    matrix_file = "models/tm_test.p"
     n_train_samples = 5000  # Number of words to train translation matrix.
     n_translations = 1 # (max) number of translation variants per word (default=1)
 
-    #dict_file = "models/lowercase/top10ken_trans_yandex.txt"
-    dict_file = sys.argv[1]
+    dict_file = "models/truecase/top10ken_trans_yandex.txt" # file that contains en word and de translation(s)
 
-    alphas = [0.1, 0.2,  0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]  # linear regression solver regularization param
-
-    #modelPathEn = 'models/truecase/mono_200_en.bin'
-    #modelPathDe = 'models/truecase/mono_200_de.bin'
-
-    modelPathEn = sys.argv[2]
-    modelPathDe = sys.argv[3]
+    modelPathEn = 'models/truecase/mono_800_en.bin' # en word2vec model
+    modelPathDe = 'models/truecase/mono_200_de.bin' # de word2vec model
 
     # Evaluation
-    n_test_samples = 100 # number of test samples (subsequent words not used in training)
+    n_test_samples = 1000 # number of test samples (subsequent words not used in training)
     n_test_translations = 1 # number of translation variants to be considered as gold standard
     k_nearest = 5 # number of k nearest words of translated vector to be considered.
-
-
     # ------------- End params -------------#
+
+    if len(sys.argv) > 1:
+        try:
+            dict_file, modelPathEn, modelPathDe, matrix_file = sys.argv[1:]
+            print "Running with command-line arguments."
+        except:
+            print "Error arguments:"
+            print "<dict_file> <model_en> <model_de> <matrix_output_file>"
+    else:
+        print "Running with parameters specified in file."
 
 
     # Load word2vec trained models
@@ -162,6 +167,7 @@ if __name__ == '__main__':
 
 
     # Split training and test set
+    # Makes sure that only valid samples (with vectors) are selected.
     train_samples, test_samples = get_train_test_sets(dict_file, n_translations, n_train_samples, n_test_samples, gensimModelDe)
 
 
@@ -181,40 +187,34 @@ if __name__ == '__main__':
         X[i] = wordRepEn
         Y[i] = wordRepDe
 
-    # parameter search
-    accuracies = []
-    average_sims = []
 
-    for j, alpha in enumerate(alphas):
-        print "Training with alpha=", alpha
-        translation_matrix = learn_translation_matrix(X, Y, alpha)
+    print "Training matrix with alpha cross-validation"
+    translation_matrix, alpha = learn_translation_matrix(X, Y)
 
-        print "Saving matrix"
-        # Store translation matrix
-        #pickle.dump(translation_matrix,open(matrix_file,'w'))
+    print "Saving matrix"
+    # Store translation matrix
+    pickle.dump(translation_matrix,open(matrix_file,'w'))
 
-        print "Evaluating"
-        # evaluation
-        similarities, predicted_translations = evaluate(test_samples, gensimModelEn, gensimModelDe, translation_matrix, k_nearest=k_nearest, n_translations=n_test_translations)
-        accuracy, average_sim = calculate_accuracy(similarities)
+    print "Evaluating"
+    # evaluation
+    similarities, predicted_translations, ranks = evaluate(test_samples, gensimModelEn, gensimModelDe,
+                                                           translation_matrix, k_nearest=k_nearest,
+                                                           n_translations=n_test_translations)
+    r_acc, average_sim, average_rank = calculate_acc_avgsim(similarities, ranks)
 
-        for i, (s, t) in enumerate(test_samples):
-            print "Test word: ", s
-            print "Gold: ", t
-            print "Cand: ", predicted_translations[i]
-            print "Sim: ", similarities[i]
+    # print test results
+    # comment out if not desired
+    for i, (s, t) in enumerate(test_samples):
+        print "Test word: ", s
+        print "Gold: ", t
+        print "Cand: ", predicted_translations[i]
+        print "Sim: ", similarities[i]
 
-        print "----------"
-        print "Alpha: ", alpha
-        print "Accuracy: ", accuracy
-        print "Average similarity: ", average_sim
+    # print final results
+    print "----------"
+    print "Samples train/test: ", n_train_samples, n_test_samples
+    print "Alpha: ", alpha
+    print "Ranked accuracies: ", r_acc
+    print "Average similarity: ", average_sim
+    print "Average rank: ", average_rank
 
-        accuracies.append(accuracy)
-        average_sims.append(average_sim)
-
-    best_index = np.argmax(accuracies)
-
-    print "alpha - accuracy - average similarity"
-    for j, alpha in enumerate(alphas):
-        best = "*" if best_index == j else ""
-        print alpha, accuracies[j], average_sims[j], best
