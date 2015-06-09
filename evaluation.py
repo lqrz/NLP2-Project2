e00111b8dd1feaee85763495bde98c56bdd1fda6
lqrz <lautaro.quiroz@gmail.com>
2015-06-09 18:44:13 +0200

    argv

--- evaluation.py
+++ evaluation.py
@@ -4,28 +4,38 @@ import pickle
 from CorpusReader import CorpusReader
 from nltk.corpus import PlaintextCorpusReader
 from nltk import FreqDist
-
+import sys
 
 if __name__ == '__main__':
-	pickled = True
 
-	if not pickled:
-		corpus = CorpusReader('corpus/truecased/5k/train/model/phrase-table.gz')
+	if len(sys.argv)==4:
+		phraseTablePath = sys.argv[1]
+		testCorpusFolder = sys.argv[2]
+		pickled = sys.argv[3]
+	else:
+		print 'Usage: 1)Phrase table path, 2) Test corpus folder, 3) pickled'
+	if pickled=='False':
+		corpus = CorpusReader(phraseTablePath)
 
 		l = set()
 
 		for sentence in corpus.get_raw_corpus():
-			l.add(sentence.split('|||')[0])
+			l.add(sentence.split('|||')[0].strip())
 		pickle.dump(l,open('original_phrasetable.p','wb'))
+	
+		test_corpus = PlaintextCorpusReader(testCorpusFolder, 'test.true.de')
+		fd = FreqDist(test_corpus.words())
+		pickle.dump(fd, open('test_freqdict.p','wb'))
+
 	else:
 		print 'Getting pickled table set'
 		l = pickle.load(open('original_phrasetable.p', 'rb'))
-
+		fd = pickle.load(open('test_freqdict.p','rb'))
 	print 'Getting test corpus'
 	# test_corpus = PlaintextCorpusReader('corpus/testing/','test.true.de')
-	test_corpus = PlaintextCorpusReader('corpus/testing/', 'test.true.de')
-	fd = FreqDist(test_corpus.words())
 
-	coverage = len(fd.keys() - l)
+	oovs = len(set(fd.keys()) - l)
+	oovs_perc = oovs / len(fd.keys())
 
-	print 'Word coverage', coverage
+	print 'oovs: ', oovs, oovs_perc, '\n'
+	print 'Total words in set: ', len(fd.keys())
