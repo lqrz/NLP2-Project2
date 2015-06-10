afe81478ab2d7704207cb14f4dfe49423b6fde28
lqrz <lautaro.quiroz@gmail.com>
2015-06-10 21:30:15 +0200

    name changes

--- retune50.sh
+++ retune50.sh
@@ -1,13 +1,23 @@
+#!/bin/bash
 for p in $(ls | grep '^wordsToReplace5')
 	do
+		NEWCORPUS="test.true.50.de"
+		PHRASETABLE="phrase-table-50.gz"
+
+		echo $NEWCORPUS
 		echo "making replacements in test corpus: $p"
-		cp corpus/testing/test.true.de test.true."$p".de
+
+		cp corpus/testing/test.true.de $NEWCORPUS
+
 		for oov in $(cat "$p")
 			do
-				sed -i "s/\<$oov\>/##$oov/g" test.true."$p".de
+				sed -i "s/\<$oov\>/##$oov/g" $NEWCORPUS
 			done
 		echo "generating second table"
+
 		python generateSecondTable.py /home/wechsler/NLP2-Project2/models/truecase/ "$p" /home/wechsler/NLP2-Project2/models/truecase/de-en/yandex/ /home/wechsler/NLP2-Project2/models/truecase/en-de/yandex/
+
+		mv "secondPhraseTable$p.gz" $PHRASETABLE
 
 		echo "retuning Moses"
 	done
