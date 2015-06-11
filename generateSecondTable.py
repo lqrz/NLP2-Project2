83c95ec6f64f95fc9fc0a124fdf17cd5260efcbe
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2015-06-11 20:38:58 +0200

    fixes

--- generateSecondTable.py
+++ generateSecondTable.py
@@ -1,8 +1,9 @@
 __author__ = 'lqrz'
 import sys
-from itertools import combinations
+# from itertools import combinations
 import pickle
 from gensim import gensim
+# import gensim
 import numpy as np
 import codecs
 from os import walk
@@ -22,14 +23,14 @@ if __name__ == '__main__':
     if len(sys.argv) == 5:
         modelsPath = sys.argv[1]
         oovFilename = sys.argv[2]
-	directPath = sys.argv[3]
-	inversePath = sys.argv[4]
+        directPath = sys.argv[3]
+        inversePath = sys.argv[4]
     elif len(sys.argv) > 1 :
         print 'Error! missing parameters'
 
-    dimensions = [200,400,800]
+    # dimensions = [200,400,800]
 
-    # oovFilenames = ['oovs24.5998608212.p'] # TODO: Add pickle filenames
+    # oovFilename = 'wordsToReplace20.6985702836.txt' # TODO: For debug. Comment.
 
     oovFilenames = []
     if oovFilename == 'False':
@@ -45,26 +46,31 @@ if __name__ == '__main__':
     tmFilenames = set()
     modelFilenames = set()
 
-    for d in dimensions:
-        d1 = str(d)
-        tmFilenames.add(('mono_'+d1+'_de.bin', 'mono_'+d1+'_en.bin', 'tm_'+ d1 +'_'+ d1 +'.p', 'tm_'+ d1 +'_'+ d1 +'.p'))
+    # for d in dimensions:
+    #     d1 = str(d)
+    #     tmFilenames.add(('mono_'+d1+'_de.bin', 'mono_'+d1+'_en.bin', 'tm_'+ d1 +'_'+ d1 +'.p', 'tm_'+ d1 +'_'+ d1 +'.p'))
+    #
+    # for p in combinations(dimensions,2):
+    #     de = str(p[0])
+    #     en = str(p[1])
+    #     tmFilenames.add(('mono_'+de+'_de.bin', 'mono_'+en+'_en.bin', 'tm_'+de+'_'+en+'.p', 'tm_'+en+'_'+de+'.p'))
 
-    for p in combinations(dimensions,2):
-        de = str(p[0])
-        en = str(p[1])
-        tmFilenames.add(('mono_'+de+'_de.bin', 'mono_'+en+'_en.bin', 'tm_'+de+'_'+en+'.p', 'tm_'+en+'_'+de+'.p'))
 
+    tmFilenames = [('mono_800_de.bin', 'mono_400_en.bin', 'tm_800_400.p', 'mono_400_de.bin', 'mono_800_en.bin', 'tm_800_400.p')]
 
-    tmFilenames = [('mono_800_de.bin', 'mono_400_en.bin', 'tm_800_400.p', 'tm_400_800.p')]
-#    directPath = 'models/'
-#    inversePath = 'models/'
-#    modelsPath = 'models/'
+    #TODO: this are debug param values. Comment.
+    # tmFilenames = [('mono_800_de.bin', 'mono_200_en.bin', 'tm_800_200.p', 'mono_200_de.bin', 'mono_800_en.bin', 'tm_800_200.p')]
+    # directPath = 'models/de-en/'
+    # inversePath = 'models/en-de/'
+    # modelsPath = 'models/'
 
     for i,t in enumerate(tmFilenames):
         deModel = gensim.models.Word2Vec.load_word2vec_format(modelsPath+t[0],binary=True)
         enModel = gensim.models.Word2Vec.load_word2vec_format(modelsPath+t[1],binary=True)
         directMatrix = pickle.load(open(directPath + t[2]))
-        inverseMatrix = pickle.load(open(inversePath + t[3]))
+        deModelInverse = gensim.models.Word2Vec.load_word2vec_format(modelsPath+t[3],binary=True)
+        enModelInverse = gensim.models.Word2Vec.load_word2vec_format(modelsPath+t[4],binary=True)
+        inverseMatrix = pickle.load(open(inversePath + t[5]))
 
     noTranslationWords = set()
 
@@ -94,23 +100,25 @@ if __name__ == '__main__':
                 candidates = enModel.most_similar(positive=[calculatedRep], topn=nCandidates)
 
                 totalProb = 0
+                normalizedCandidates = set()
                 for wCand, wCos in candidates:
                     totalProb += wCos
                     try:
-                        inverseRep = enModel[wCand]
+                        inverseRep = enModelInverse[wCand]
                         inverseCalculatedRep = calculatedRep = np.dot(inverseMatrix, inverseRep)
-                        inverseCandidates = deModel.most_similar(positive=[inverseCalculatedRep], topn=nCandidates)
+                        inverseCandidates = deModelInverse.most_similar(positive=[inverseCalculatedRep], topn=nCandidates)
                         tot = 0
-                        if any(oov in trans for trans in inverseCandidates):
+                        if any(oov in t for t in inverseCandidates):
                             idx = next(i for i, (trans, _) in enumerate(inverseCandidates) if trans == oov)
-                            tot += sum([cos for _,cos in inverseCandidates])
-                            inverseProb = inverseCandidates[idx][1] / float(tot)
+                            tot += sum([abs(cos) for _,cos in inverseCandidates])
+                            inverseProb = float(inverseCandidates[idx][1]) / tot
                         else:
                             inverseProb = 0
-                    except:
+                    except KeyError:
                         inverseProb = 0
-
-                for wCand, wCos in candidates:
+                        foutLog.write('Couldnt find inverse candidate representation for OOV: ' + oov + ' inv.Cand: ' + wCand + '\n')
+                    normalizedCandidates.add((wCand, wCos, inverseProb))
+                for wCand, wCos, inverseProb in normalizedCandidates:
                     convOOV = '##' + oov
                     sep = '|||'
                     alig = '0-0'
