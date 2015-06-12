5fac8f4f136d3a7f6e0078f38339a67c5187da25
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2015-06-12 13:44:38 +0200

    encoding and add to natural oovs

--- changeCorpusOOvs.py
+++ changeCorpusOOvs.py
@@ -5,6 +5,7 @@ from nltk.corpus import PlaintextCorpusReader
 from nltk import FreqDist
 import random
 import codecs
+import gensim
 
 if __name__ == '__main__':
 
@@ -13,14 +14,23 @@ if __name__ == '__main__':
     maxFreq = 50
 
     testCorpusPath = 'data/test.true.de'
-    if len(sys.argv) == 2:
+    naturalOOVsFilename = 'naturalOOVs.txt'
+    modelPath = 'models/mono_800_de.bin'
+    if len(sys.argv) == 4:
         testCorpusPath = sys.argv[1]
+        naturalOOVsFilename = sys.argv[2]
+        modelPath = sys.argv[3]
 
     idx = testCorpusPath.rfind('/') + 1
     folder = testCorpusPath[0:idx]
     filename = testCorpusPath[idx:]
 
-    corpus = PlaintextCorpusReader(folder, filename)
+    corpus = PlaintextCorpusReader(folder, filename, encoding='utf-8')
+    naturalCorpus = PlaintextCorpusReader('./',naturalOOVsFilename, encoding='utf-8')
+
+    model = gensim.models.Word2Vec.load_word2vec_format(modelPath,binary=True)
+
+    naturalOOVs = set(naturalCorpus.words())
 
     depunct = [w for w in corpus.words() if w.isalpha()]
 
@@ -32,12 +42,26 @@ if __name__ == '__main__':
 
     filteredWords = [(w,f) for w,f in mostFrequentWords if f>=minFreq and f<=maxFreq ]
 
-    percs = [20,50,70]
+    # base = sum([f for w,f in fd.items() if w in naturalOOVs]) / float(totalLen)
+    base = sum([fd[w] for w in naturalOOVs]) / float(totalLen)
+
+    percs = [int(x) - int(base*100) for x in [20, 50, 70]]
     tot = 0
     candidates = set()
     for p in percs:
         while tot < p:
             candidate = random.choice(filteredWords)
+
+            # check we are adding a new unknown to the natural set
+            if candidate[0] in naturalOOVs:
+                continue
+
+            # check we can get a translation for it
+            try:
+                model[candidate[0]]
+            except KeyError:
+                continue
+
             tot += candidate[1]*100 / float(totalLen)
             candidates.add(candidate[0]) # add word to remove from phrase table or corpus
         fout = codecs.open('wordsToReplace'+str(tot)+'.txt','w','utf-8')
